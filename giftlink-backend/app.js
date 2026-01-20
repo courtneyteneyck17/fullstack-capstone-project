b975e989f1525e645469dcee6a73727fd6d2990a
Courtney Ten Eyck <courtneyteneyck17@gmail.com>
2026-01-19 19:25:59 -0800

    Import giftRoutes and searchRoutes in app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -23,10 +23,10 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+//{{const searchRoutes = require('./routes/searchRoutes');}}
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+//{{app.use('/api/search', searchRoutes);}}
 
 
 const pinoHttp = require('pino-http');
